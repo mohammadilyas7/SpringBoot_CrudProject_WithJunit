788642f82ef063226495668a7229722d4f72e69a
ilyas78874@gmail.com <ilyas78874@gmail.com>
2024-10-19 12:35:09 +0530

    UserServiceTest testGetUserDetails

--- User-Service/src/test/java/example/User_Service/controller/testService/UserServiceTest.java
+++ User-Service/src/test/java/example/User_Service/controller/testService/UserServiceTest.java
@@ -3,23 +3,16 @@ package example.User_Service.controller.testService;
 import example.User_Service.model.User;
 import example.User_Service.repository.UserRepository;
 import example.User_Service.service.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import java.util.Optional;
 
-//import static jdk.internal.org.objectweb.asm.util.CheckClassAdapter.verify;
+import static org.bson.assertions.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 
 public class UserServiceTest {
     @InjectMocks
@@ -37,11 +30,24 @@ public class UserServiceTest {
         user.setEmail("test@example.com");
         user.setName("Test User");
 
-        // Mock the userRepository to simulate saving the user
         when(userRepository.findById(user.getEmail())).thenReturn(Optional.empty()); // No existing user
         String result = userService.register(user);
 
         assertEquals("User Successfully Register", result);
-//        verify(userRepository, times(1)).save(user); // Verify that save was called
+    }
+    @Test
+    public void testGetUserDetails(){
+        String email = "ilyas78874@gmail.com";
+        User user = new User();
+        user.setEmail(email);
+        user.setName("Test User");
+        // Mock the repository to return an existing user
+        when(userRepository.findById(email)).thenReturn(Optional.of(user));
+
+        User result = userService.getUserDetails(email);
+
+        assertNotNull(result);
+        assertEquals("Test User", result.getName());
+        assertEquals(email, result.getEmail());
     }
 }
